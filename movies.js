8a350c211c3a19aa4e250bcbff87208bd0fa0e6b
Md Tamim JD <tamimoli75285@gmail.com>
2026-01-07 17:37:43 +0600

    Fix thumbnail URLs for movie entries
    Updated thumbnail URLs for the movies in the list.

--- movies.js
+++ movies.js
@@ -7,7 +7,7 @@ const movies = [
         year: "2020",
         rating: "8.8",
         views: 1200,
-        thumbnail: "https://drive.google.com/uc?export=view&id=1G1cegCi-ZTwcvG_keumN-6Wpia1KzvPC",
+        thumbnail: "https://drive.google.com/uc?export=view&id=1tCCBHRgU-mmstp0_yfidKIS6YI6hvYmQ",
         videoUrl: "https://drive.google.com/file/d/1-UbksDPj86PjtDL716X7kpvfkTxCCP90/preview", // Replace with GDrive Embed Link
         downloadUrl: "https://drive.google.com/uc?export=download&id=1-UbksDPj86PjtDL716X7kpvfkTxCCP90"
     },
@@ -67,5 +67,6 @@ const movies = [
     }
 
 ];
+
 
 
